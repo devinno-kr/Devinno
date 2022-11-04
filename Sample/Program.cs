3a72323f17a2dacd30fdf8edf54c49864b049839
devinno <devinno@devinno.kr>
2022-11-04 13:02:38 +0900

    -

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Devinno.Communications.Modbus.TCP;
 using Devinno.Data;
+using Devinno.Extensions;
 using System;
+using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -9,18 +11,22 @@ namespace Sample
 {
     class Program
     {
+        static string PER(double v) => v.ToString("0%");
         static void Main(string[] args)
         {
-            ModbusTCPSlaveBase tcp = new ModbusTCPSlaveBase();
-            ModbusTCPSlaveBase tcp2 = new ModbusTCPSlaveBase();
+    var ba = new byte[1024];
+    var M = new BitMemories("M", ba);
+    var WM = new WordMemories("WM", ba);
 
-            tcp.Start();
-            Thread.Sleep(5000);
-            tcp.Stop();
-            Thread.Sleep(5000);
-            tcp2.Start();
-            Thread.Sleep(5000);
-            tcp2.Stop();
+    var s = "";
+    WM[0] = 0x1234;
+    for (int i = 0; i < 16; i++) s += (M[i] ? "1" : "0");
+
+    Console.WriteLine($" M Count : {M.Size}");
+    Console.WriteLine($"WM Count : {WM.Size}");
+    Console.WriteLine("");
+    Console.WriteLine($"WM000 = {WM[0].ToString("X4")}");
+    Console.WriteLine($"M0:15 = {s}");
 
             Console.ReadKey();
         }
