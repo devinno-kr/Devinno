62e3561a5fba2d8f9f9499ffe1e10d1838ddff63
devinno <devinno@devinno.kr>
2022-11-04 17:01:14 +0900

    -

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -11,23 +11,8 @@ namespace Sample
 {
     class Program
     {
-        static string PER(double v) => v.ToString("0%");
         static void Main(string[] args)
         {
-    var ba = new byte[1024];
-    var M = new BitMemories("M", ba);
-    var WM = new WordMemories("WM", ba);
-
-    var s = "";
-    WM[0] = 0x1234;
-    for (int i = 0; i < 16; i++) s += (M[i] ? "1" : "0");
-
-    Console.WriteLine($" M Count : {M.Size}");
-    Console.WriteLine($"WM Count : {WM.Size}");
-    Console.WriteLine("");
-    Console.WriteLine($"WM000 = {WM[0].ToString("X4")}");
-    Console.WriteLine($"M0:15 = {s}");
-
             Console.ReadKey();
         }
     }
