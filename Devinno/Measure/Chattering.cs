56849e117d17b18cba9551a621c4c93ca61cab0f
devinno <devinno@devinno.kr>
2023-09-27 15:16:06 +0900

    -

--- Devinno/Measure/Chattering.cs
+++ Devinno/Measure/Chattering.cs
@@ -15,13 +15,14 @@ namespace Devinno.Measure
         #region Properties
         public bool State { get; private set; }
         public int ChatteringTime { get; set; }
+        public ChatteringMode Mode { get; set; } = ChatteringMode.Both;
         #endregion
-        
+
         #region Member Variable
         bool prevState = false;
         DateTime prevTime = DateTime.Now;
         #endregion
-        
+
         #region Constructor
         public Chattering()
         {
@@ -34,28 +35,107 @@ namespace Devinno.Measure
         #region Set
         public void Set(bool Value)
         {
-            bool v = Value;
-            if (this.prevState == v)
+            if (Mode == ChatteringMode.Both)
             {
-                if ((DateTime.Now - prevTime).TotalMilliseconds >= this.ChatteringTime)
+                #region Both
+                bool v = Value;
+                if (this.prevState == v)
                 {
-                    if (this.State != v)
+                    if ((DateTime.Now - prevTime).TotalMilliseconds >= this.ChatteringTime)
                     {
-                        if (StateChanged != null) StateChanged.Invoke(this, new ChatteringStateChangedEventArgs(v));
-                        this.State = v;
+                        if (this.State != v)
+                        {
+                            StateChanged?.Invoke(this, new ChatteringStateChangedEventArgs(v));
+                            this.State = v;
+                        }
                     }
                 }
+                else
+                {
+                    this.prevState = v;
+                    prevTime = DateTime.Now;
+                }
+                #endregion
             }
-            else
+            else if (Mode == ChatteringMode.On)
             {
-                this.prevState = v;
-                prevTime = DateTime.Now;
+                #region High
+                bool v = Value;
+                if (!v)
+                {
+                    prevTime = DateTime.Now;
+                    if (this.State != v)
+                    {
+                        StateChanged?.Invoke(this, new ChatteringStateChangedEventArgs(v));
+                        this.State = v;
+                    }
+                    this.prevState = v;
+                }
+                else
+                {
+                    if (this.prevState != v)
+                    {
+                        prevTime = DateTime.Now;
+                        this.prevState = v;
+                    }
+                    else
+                    {
+                        if ((DateTime.Now - prevTime).TotalMilliseconds >= this.ChatteringTime)
+                        {
+                            if (this.State != v)
+                            {
+                                StateChanged?.Invoke(this, new ChatteringStateChangedEventArgs(v));
+                                this.State = v;
+                            }
+                        }
+                    }
+                }
+
+                #endregion
+            }
+            else if (Mode == ChatteringMode.Off)
+            {
+                #region Off
+                bool v = Value;
+                if (v)
+                {
+                    prevTime = DateTime.Now;
+                    if (this.State != v)
+                    {
+                        StateChanged?.Invoke(this, new ChatteringStateChangedEventArgs(v));
+                        this.State = v;
+                    }
+                    this.prevState = v;
+                }
+                else
+                {
+                    if (this.prevState != v)
+                    {
+                        prevTime = DateTime.Now;
+                        this.prevState = v;
+                    }
+                    else
+                    {
+                        if ((DateTime.Now - prevTime).TotalMilliseconds >= this.ChatteringTime)
+                        {
+                            if (this.State != v)
+                            {
+                                StateChanged?.Invoke(this, new ChatteringStateChangedEventArgs(v));
+                                this.State = v;
+                            }
+                        }
+                    }
+                }
+                #endregion
             }
         }
         #endregion
         #endregion
     }
 
+    #region [enum] ChatteringMode
+    public enum ChatteringMode { Both, On, Off }
+    #endregion
     #region [class] ChatteringStateChangedEventArgs
     public class ChatteringStateChangedEventArgs : EventArgs
     {
