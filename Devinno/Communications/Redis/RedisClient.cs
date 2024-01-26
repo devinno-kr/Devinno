76f650a3f7fd19178e7bb064bd48f66942387083
devinno <devinno@devinno.kr>
2024-01-26 11:57:14 +0900

    -

--- Devinno/Communications/Redis/RedisClient.cs
+++ Devinno/Communications/Redis/RedisClient.cs
@@ -22,14 +22,32 @@ namespace Devinno.Communications.Redis
         public int Timeout { get; set; } = 1000;
 
         /// <summary>
+        /// 통신 시작 여부
+        /// </summary>
+        public bool IsStart { get; private set; }
+
+        /// <summary>
         /// 접속 여부
         /// </summary>
         public bool IsConnected => conn != null && conn.IsConnected;
+
+        public ConnectionMultiplexer Redis => conn;
+        public IDatabase Database => db;
+        public IServer Server => svr;
         #endregion
 
         #region Member Variable
         ConnectionMultiplexer conn = null;
         IDatabase db;
+        IServer svr;
+
+        System.Threading.Thread th;
+        DateTime prev = DateTime.Now;
+        #endregion
+
+        #region Event
+        public event EventHandler Connected;
+        public event EventHandler Disconnected;
         #endregion
 
         #region Constructor
@@ -40,35 +58,79 @@ namespace Devinno.Communications.Redis
 
         #region Method
         #region Set
-        public void Set(string key, int value) => SetValue(key, value);
-        public void Set(string key, long value) => SetValue(key, value);
         public void Set(string key, string value) => SetValue(key, value);
+        public void Set(string key, int value) => SetValue(key, value);
+        public void Set(string key, uint value) => SetValue(key, value);
+        public void Set(string key, double value) => SetValue(key, value);
         public void Set(string key, byte[] value) => SetValue(key, value);
+        public void Set(string key, bool value) => SetValue(key, value);
+        public void Set(string key, long value) => SetValue(key, value);
+        public void Set(string key, ulong value) => SetValue(key, value);
+        public void Set(string key, float value) => SetValue(key, value);
 
-        public void SetValue(string key, RedisValue value)
+        public bool SetValue(string key, RedisValue value)
         {
-            if (conn != null && conn.IsConnected && db != null)
-            {
-                db.StringSet(key, value);
-            }
+            bool ret = false;
+            if (conn != null && conn.IsConnected && db != null) ret = db.StringSet(key, value);
+            return ret;
         }
+
+        public void JsonSet(string key, object value) => SetValue(key, Serialize.JsonSerialize(value));
         #endregion
         #region Get
-        public long GetInt64(string key) => (long)GetValue(key);
-        public int GetInt32(string key) => (int)GetValue(key);
         public string GetString(string key) => (string)GetValue(key);
-        public  byte[] GetBytes(string key) => (byte[])GetValue(key);
+        public int GetInt32(string key) => (int)GetValue(key);
+        public uint GetUInt32(string key) => (uint)GetValue(key);
+        public double GetDouble(string key) => (double)GetValue(key);
+        public byte[] GetBytes(string key) => (byte[])GetValue(key);
+        public bool GetBoolean(string key) => (bool)GetValue(key);
+        public long GetInt64(string key) => (long)GetValue(key);
+        public ulong GetUInt64(string key) => (ulong)GetValue(key);
+        public float GetSingle(string key) => (float)GetValue(key);
 
         public RedisValue GetValue(string key)
         {
-            string ret = null;
+            RedisValue ret = RedisValue.Null;
             if (conn != null && conn.IsConnected && db != null)
             {
                 ret = db.StringGet(key);
             }
             return ret;
         }
+
+        public T JsonGet<T>(string key) => Serialize.JsonDeserialize<T>(GetString(key));
         #endregion
+
+        #region Hash
+        #region Set
+        public void HashSet(string key, HashEntry[] values)
+        {
+            if (conn != null && conn.IsConnected && db != null) db.HashSet(key, values);
+        }
+        #endregion
+        #region Get
+        public HashEntry[] HashGet(string key)
+        {
+            HashEntry[] ret = null;
+            if (conn != null && conn.IsConnected && db != null)
+            {
+                ret = db.HashGetAll(key);
+            }
+            return ret;
+        }
+
+        public RedisValue HashGet(string key, string field)
+        {
+            RedisValue ret = RedisValue.Null;
+            if (conn != null && conn.IsConnected && db != null)
+            {
+                ret = db.HashGet(key, field);
+            }
+            return ret;
+        }
+        #endregion
+        #endregion
+
         #region KeyExists
         public bool? KeyExists(string key)
         {
@@ -80,22 +142,81 @@ namespace Devinno.Communications.Redis
             return ret;
         }
         #endregion
-        #region Open
-        public void Open()
+
+        #region Start
+        public void Start()
         {
-            ConfigurationOptions conf = new ConfigurationOptions() { EndPoints = { Host }, ConnectTimeout = Timeout };
-            conn = ConnectionMultiplexer.Connect(conf);
-            db = conn.GetDatabase();
+            try
+            {
+                th = new System.Threading.Thread(new System.Threading.ThreadStart(run));
+                th.IsBackground = true;
+                th.Start();
+            }
+            catch (Exception) { }
         }
         #endregion
-        #region Close
-        public void Close()
+        #region Stop
+        public void Stop()
         {
-            if (conn != null) conn.Dispose();
+            IsStart = false;
+        }
+        #endregion
+
+        #region Thread
+        void run()
+        {
+            IsStart = true;
+ 
+            while (IsStart)
+            {
+                if (conn == null || (conn != null && !conn.IsConnected))
+                {
+                    try
+                    {
+                        ConfigurationOptions conf = new ConfigurationOptions() { EndPoints = { Host }, ConnectTimeout = Timeout };
+                        conn = ConnectionMultiplexer.Connect(conf);
+                        db = conn.GetDatabase();
+
+                        var endpoint = conn.GetEndPoints().Single();
+                        svr = conn.GetServer(endpoint);
+
+                    }
+                    catch (Exception ex) { }
+                }
+                else
+                {
+                    if (db != null)
+                    {
+                        if (db.KeyExists("test-alive")) { var v = db.StringGet("test-alive"); }
+                        else db.StringSet("test-alive", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                        prev = DateTime.Now;
+                    }
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            if (conn != null && conn.IsConnected)
+            {
+                try
+                {
+                    conn.Close();
+                }
+                catch (Exception ex) { }
+            }
+
+            svr = null;
+            db = null;
+            conn = null;
+
+            
         }
         #endregion
         #endregion
 
     }
 
+    #region attribute : RedisIgnore
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class RedisIgnoreAttribute : Attribute { }
+    #endregion}
 }
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Devinno.Collections;
 using Devinno.Communications.Modbus.TCP;
+using Devinno.Communications.Redis;
 using Devinno.Communications.Restful;
 using Devinno.Communications.TextComm.RTU;
 using Devinno.Data;
@@ -23,84 +24,16 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            /*
-            ModbusTCPMaster mb = new ModbusTCPMaster { RemoteIP = "172.30.1.123", AutoStart = true };
-            mb.AutoWordRead_FC3(1, 1, 0x7000, 10);
-            mb.WordReadReceived += (o, s) =>
-            {
-                Console.CursorLeft = 0; 
-                Console.CursorTop = 0; 
-                Console.WriteLine(s.ReceiveData[0].ToString().PadLeft(5));
-            };
+            var c = new RedisClient { Host = "127.0.0.1" };
+            c.Start();
+            Thread.Sleep(1000);
 
-            while (true)
-            {
-                var i = Convert.ToByte(DateTime.Now.Second % 16);
-                mb.ManualWordWrite_FC6(1, 1, 0x7000, 0);
-                mb.ManualWordBitSet_FC26(1, 1, 0x7000, i, true);
-                Thread.Sleep(1000);
-            }
-            */
+            c.Set("test_byte", new byte[] { 1, 2, 3, 4, 5 });
+            var ba = c.GetBytes("test_byte");
 
-            /*
-            ModbusTCPSlave mb = new ModbusTCPSlave { Slave = 1 };
-            BitMemories P = new BitMemories("P", new byte[512]);
-            BitMemories M = new BitMemories("M", new byte[512]);
-            WordMemories C = new WordMemories("C", new byte[8192]);
-            WordMemories D = new WordMemories("D", new byte[8192]);
-            mb.BitAreas.Add(0x0000, P);
-            mb.BitAreas.Add(0x1000, M);
-            mb.WordAreas.Add(0x6000, C);
-            mb.WordAreas.Add(0x7000, D);
-            mb.Start();
+            Console.ReadKey();
 
-            M[0] = true;
-            M[2] = true;
-            M[4] = true;
-            */
-
-            /*
-            var comm = new TextCommRTUMaster { Port = "COM13", Baudrate = 115200, AutoStart = true };
-            comm.MessageReceived += (o, s) =>
-            {
-                Console.WriteLine(s.Command + " : " + s.Message);
-            };
-            comm.AutoSend(1, 1, 4, "");
-            byte cmd = 1;
-            */
-
-            /*
-            var lns = File.ReadAllLines(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "tet.json")).Select(x => x.Trim());
-            var lns2 = lns.Where(x => x.StartsWith("\"type\":")).Select(x => x.Split(':')).ToLookup(x => x[1]);
-            var ks = string.Concat(lns2.Select(X => X.Key + ","));
-
-            var via = new Dictionary<string, object>();
-            via.Add("test1", 10);
-            via.Add("test2", 3.141592);
-            via.Add("test3", true);
-            via.Add("test4", new int[] { 1, 2, 3 });
-            via.Add("test5", "text");
-
-            var s = Serialize.JsonSerialize(via);
-            */
-
-            var dic = new EventDictionary<int, string>();
-            dic.Changed += (o, s) => Console.WriteLine(dic.Count.ToString());
-
-            int i = 0;
-            while (true)
-            {
-                dic.Add(i, "");
-                Thread.Sleep(1000);
-                i++;
-            }
         }
 
-        class V 
-        {
-            public short Data1;
-            public string Data2;
-            public float Data3;
-        }
     }
 }
